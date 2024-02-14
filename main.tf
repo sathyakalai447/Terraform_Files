5312ecb45724ef3445c79feafe7a61658d3a1499
sathyakalai447 <75484949+sathyakalai447@users.noreply.github.com>
2024-02-14 15:52:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,14 @@
 terraform {
+  required_version = "~> 1.0" 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-
-  backend "remote" {
-    organization = "TerraformcloudPocs"
-
-    workspaces {
-      name = "AWSTerrafromcloudPOCs"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 }
+
+
 provider "aws" {
   region = "us-east-1"
 }
